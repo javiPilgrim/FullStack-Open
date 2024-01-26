1b46725e8bc8d2af3d4f744cfa8d51269208b8de
javimacias@gmail.com <javimacias@gmail.com>
2024-01-26 13:06:14 +0100

    phonebook exercise 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const nextName = {
+      name: newName
+    }
+    setPersons(persons.concat(nextName))
+    setNewName("")
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>debug: {newName}</div>
+      <ul>
+        {persons.map((person) => 
+          <li key={person.name}>{person.name}</li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
