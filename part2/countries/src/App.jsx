14423acc60c487786a2d7f037c14146dc58afaf2
javimacias@gmail.com <javimacias@gmail.com>
2024-02-09 10:30:50 +0100

    hiding personal key

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import countryService from './services/countries.jsx'
-import Countries from './components/Countries.jsx'
+import Countries from './components/countries.jsx'
 import './App.css'
 
 
