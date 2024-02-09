424b3552156f8a9b8a9b10ed5821d826cb9454e6
javimacias@gmail.com <javimacias@gmail.com>
2024-02-09 11:17:37 +0100

    exercise 3.3

--- part3/Backend/index.js
+++ part3/Backend/index.js
@@ -30,6 +30,13 @@ app.get("/info",(request, response)=>{
     <p>${now}</p>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if(person) response.json(person)
+  else response.status(404).end()
+})
+
 
 app.get("/api/persons", (request, response)=>{
     response.send(persons)
