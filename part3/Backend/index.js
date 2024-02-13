d2aa74102965992270681f247a9ed1f6627f596a
javimacias@gmail.com <javimacias@gmail.com>
2024-02-13 13:21:35 +0100

    conection font-back

--- part3/Backend/index.js
+++ part3/Backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'));
 morgan.token('req-body', (req) => JSON.stringify(req.body));
+app.use(cors())
 
 
 let now = new Date()
