424f8f750fe7609177c41f427c58e5800273a647
javimacias@gmail.com <javimacias@gmail.com>
2024-02-16 13:37:37 +0100

    add post

--- part3/Backend/index.js
+++ part3/Backend/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
-const mongoose = require('mongoose')
+const Person = require('./models/person')
+
 
 app.use(express.json())
 app.use(express.static('dist'))
@@ -14,24 +16,6 @@ const password = process.argv[2]
 let now = new Date()
 const url =
   `mongodb+srv://javipilgrim:${password}@cluster1.f5s3qfz.mongodb.net/phonebook?retryWrites=true&w=majority`
-
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-})
-
-personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Person = mongoose.model('Person', personSchema)
 
 const newId = () => {
   const numberRandom = Math.floor(Math.random() * 10000) + 1;
@@ -84,30 +68,20 @@ app.get("/api/persons", (request, response)=>{
 })
 
 app.post('/api/persons', (request, response) => {
-  const person = request.body
-  const names = persons.map(person=> person.name.toLowerCase())
+  const body = request.body
 
-  if(!person.name || person.name === ""){
-    return response.status(400).json({
-      error: 'name missing'
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  if(!person.number || person.number === ""){
-    return response.status(400).json({
-      error: 'number missing'
-    })
-  }
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
 
-  if(names.includes(person.name.toLowerCase())){
-    return response.status(400).json({
-      error: 'name must be unique'
-    })
-  }
-
-  person.id = newId()
-  persons = persons.concat(person)
-  response.json(person)
+  person.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 
