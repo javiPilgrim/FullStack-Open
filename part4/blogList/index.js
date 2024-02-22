6e690f8efdec180547ffebb73262e1444cf86cda
javimacias@gmail.com <javimacias@gmail.com>
2024-02-22 14:11:11 +0100

    conect to mongodb

--- part4/blogList/index.js
+++ part4/blogList/index.js
@@ -1,11 +1,30 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
 
-app.use(express.json())
+const password = 'mongopilgrim'
+
+const url =
+`mongodb+srv://javimacias:${password}@cluster1.f5s3qfz.mongodb.net/blogsList?retryWrites=true&w=majority&appName=Cluster1`
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+  })
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl =`mongodb+srv://javimacias:mongopilgrim@cluster2.nq61d1f.mongodb.net/blogsList?retryWrites=true&w=majority&appName=Cluster2`
+
+mongoose.connect(mongoUrl)
+
 app.use(cors())
+app.use(express.json())
 
-
+/*
 let blogs = [
     {
         title: "Los amigos",
@@ -19,26 +38,35 @@ let blogs = [
         url: "http://www.losajos.com",
         likes: 222
     }
-
 ]
-
-
-
-
+*/
 
 app.get('/api/blogs', (request, response) => {
-
+  Blog
+    .find({})
+    .then(blogs => {
       response.json(blogs)
     })
-
+})
+/*
 app.post('/api/blogs', (request, response) => {
   const blog = request.body
   console.log(blog)
   response.json(blog)
-
-
- 
 })
+*/
+
+const blog = new Blog({
+    title: "Los chorizos",
+    author: "Barcenas",
+    url: "http://www.barci.com",
+    likes: 1123
+})
+
+blog.save().then(result => {
+    console.log('blog saved!')
+    mongoose.connection.close()
+  })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
