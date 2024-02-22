9fb49ffafbb1330a7c7814e09d0fc7b3e5f6f26d
javimacias@gmail.com <javimacias@gmail.com>
2024-02-22 12:18:09 +0100

    blogList creation

---
+++ part4/blogList/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+
+let blogs = [
+    {
+        title: "Los amigos",
+        author: "pepe",
+        url: "http://www.losamigos.com",
+        likes: 123
+    },
+    {
+        title: "Los ajos",
+        author: "juanje",
+        url: "http://www.losajos.com",
+        likes: 222
+    }
+
+]
+
+
+
+
+
+app.get('/api/blogs', (request, response) => {
+
+      response.json(blogs)
+    })
+
+app.post('/api/blogs', (request, response) => {
+  const blog = request.body
+  console.log(blog)
+  response.json(blog)
+
+
+ 
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
