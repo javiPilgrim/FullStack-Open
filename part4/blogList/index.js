4035573aee47c5b1acc47fd12a77b4474a9af3b8
javimacias@gmail.com <javimacias@gmail.com>
2024-02-23 10:41:40 +0100

    exercise 4.2

--- part4/blogList/index.js
+++ part4/blogList/index.js
@@ -1,59 +1,11 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const app = require('./app') // la aplicación Express real
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
 
 
-app.use(cors())
-app.use(express.json())
-
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response, next) => {
-    const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-      .catch(error => next(error))
-  })
-
-  const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-  }
-  
-  // controlador de solicitudes con endpoint desconocido
-  app.use(unknownEndpoint)
-
-  const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.title === 'ValidationError'  || 
-        error.author === 'ValidationError' || 
-        error.url === 'ValidationError'    || 
-        error.likes === 'ValidationError') {
-      return response.status(400).json({ error: error.message })
-    }
-
-    next(error)
-  }
-  
-  // este debe ser el último middleware cargado
-  app.use(errorHandler)
 
 const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
