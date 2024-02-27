cb1e516b4f77d65e0b9ef2048814ec66fe785816
javimacias@gmail.com <javimacias@gmail.com>
2024-02-27 14:05:19 +0100

    exercise 4.9

---
+++ part4/blogList/tests/blog_api.test.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: "Los chorizos",
+    author: "Barcenas",
+    url: "http://www.barci.com",
+    likes: 1123
+  },
+  {
+    title: "Los amigos",
+    author: "pepe",
+    url: "http://www.losamigos.com",
+    likes: 123
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
+
+
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async() => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(2)
+})
+
+
+
+afterAll(() => {
+  mongoose.connection.close()
+})
