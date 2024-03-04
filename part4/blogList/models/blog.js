d0693a6af7c101371b52738734f035ca9b4b7424
javimacias@gmail.com <javimacias@gmail.com>
2024-03-04 11:33:41 +0100

    creating a user model

--- part4/blogList/models/blog.js
+++ part4/blogList/models/blog.js
@@ -33,7 +33,10 @@ mongoose.connect(url)
     likes: {
         type: Number,
         default: 0
-        
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
     }
   })
 
