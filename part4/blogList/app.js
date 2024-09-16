26ba4e93aa11edc054a4f0bf7857eabda6e4125f
Javier <javimacias@gmail.com>
2024-09-16 16:15:49 +0200

    express.static(dist)

--- part4/blogList/app.js
+++ part4/blogList/app.js
@@ -24,6 +24,7 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('dist'))
 app.use(middleware.requestLogger);
 app.use(middleware.tokenExtractor);
 app.use("/api/login", loginRouter);
