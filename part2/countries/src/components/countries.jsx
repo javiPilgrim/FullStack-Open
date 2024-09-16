623d27bcf8f972304ab99a9ac45330cea9875601
Javier <javimacias@gmail.com>
2024-09-16 12:24:13 +0200

    filter style improvements

--- part2/countries/src/components/countries.jsx
+++ part2/countries/src/components/countries.jsx
@@ -1,50 +1,55 @@
-import Country from "./country"
-import TotalWeather from "./weather"
+import Country from "./country";
+import TotalWeather from "./weather";
 
-const Countries = ({ countries, handle, showCountry}) => {
-    return (
-      <>
-        {countries.length > 1 && (
-  countries.length > 10 ? (
-    <div className="notification">Too Many matches, specify another filter</div>
-  ) : (
-    countries.map((pais) => (
-      <div key={pais.name.common}>
-        {pais.name.common}{' '}
-        <button onClick={() => handle(pais.name.common)}>Ver</button>
-        {showCountry === pais.name.common ? (
-          <Country
-            name={pais.name.common}
-            official={pais.name.official}
-            capital={pais.capital}
-            population={pais.population}
-            languages={pais.languages}
-            src={pais.flags.png}
-            alt={pais.flags.alt}
-            handle={handle}
-          />
-        ) : null}
-      </div>
-    ))
-  )
-)}
+const Countries = ({ countries, handle, showCountry }) => {
+  return (
+    <>
+      {countries.length > 1 &&
+        (countries.length > 10 ? (
+          <div className="notification">Too Many matches, specify another filter</div>
+        ) : (
+          countries.map((pais) => (
+            <div key={pais.name.common} className="country-item">
+              <span className="country-name">{pais.name.common}</span>
+              <button
+                className="country-button"
+                onClick={() => handle(pais.name.common)}
+              >
+                Ver
+              </button>
+              {showCountry === pais.name.common && (
+                <Country
+                  name={pais.name.common}
+                  official={pais.name.official}
+                  capital={pais.capital}
+                  population={pais.population}
+                  languages={pais.languages}
+                  src={pais.flags.png}
+                  alt={pais.flags.alt}
+                  handle={handle}
+                />
+              )}
+            </div>
+          ))
+        ))}
 
-        {countries.length === 1 && (
-          <>
+      {countries.length === 1 && (
+        <>
           <Country
-            name={countries['0'].name.common}
-            official = {countries['0'].name.official}
-            capital = {countries['0'].capital}
-            population = {countries['0'].population}
-            languages = {countries['0'].languages}
-            src={countries['0'].flags.png}
-            alt={countries['0'].flags.alt}
+            name={countries[0].name.common}
+            official={countries[0].name.official}
+            capital={countries[0].capital}
+            population={countries[0].population}
+            languages={countries[0].languages}
+            src={countries[0].flags.png}
+            alt={countries[0].flags.alt}
           />
-          <TotalWeather capital={countries['0'].capital} />
+          <TotalWeather capital={countries[0].capital} />
         </>
-        )}
-      </>
-    );
-  };
+      )}
+    </>
+  );
+};
 
-  export default Countries
+export default Countries;
+
